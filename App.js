8c68b1b0e5d65b2e0ebb6538eb6dc2535d5db047
Olivier <goussin.olivier@protonmail.com>
2022-11-10 17:44:51 +0000

    modified:   App.js 	modified:   app.json 	modified:   package-lock.json 	modified:   package.json 	modified:   theme.js

--- App.js
+++ App.js
@@ -1,23 +1,15 @@
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import * as React from 'react';
-
-import { provider } from 'react-redux';
-import { createStore, applyMiddleware, combineReducers } from 'redux'
-import thunk from 'redux-thunk';
-import themeReducer from "./redux/themeReducer.js";
+import colors from "./theme.js";
 
 import chatScreen from "./screens/chatScreen.js";
 import Login from "./screens/login.js"
 
-
 const AppNavigator = createStackNavigator(
   {
-    //LoginDark: loginScreenDark,
     Login: Login,
     Chat: chatScreen,
-    
-    //FirstHome: appIsLaunching
   },
   {
     headerMode: "none"
